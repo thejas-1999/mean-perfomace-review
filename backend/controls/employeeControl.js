f7436f23c1c55ba1e2e5ab38b88ac330ad7e63b1
Thejas K <143934668+thejas-1999@users.noreply.github.com>
2025-04-23 10:14:42 +0530

    create the employee creation controller

---
+++ backend/controls/employeeControl.js
@@ -0,0 +1,11 @@
+import Employee from "../models/employeeModel.js";
+
+const createEmployee = async (req, res) => {
+  try {
+    const employee = new Employee(req.body);
+    await employee.save();
+    res.status(201).json(employee);
+  } catch (error) {
+    res.status(500).json({ err: err.message });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 const mongoUrl = process.env.MONGO_URI;
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send(`Hello world`);
